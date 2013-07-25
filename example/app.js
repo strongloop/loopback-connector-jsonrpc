9ae4224ea775c3419aeb93e0c3b57816ec4e32c4
Raymond Feng <raymond@strongloop.com>
2013-07-25 10:08:19 -0700

    Add initial code

---
+++ example/app.js
@@ -0,0 +1,36 @@
+var jayson = require('jayson');
+
+// create a server
+var server = jayson.server({
+    add: function (a, b, callback) {
+        callback(null, a + b);
+    },
+    subtract: function (a, b, callback) {
+        callback(null, a - b);
+    }
+});
+
+var loopback = require("loopback");
+
+var ds = loopback.createDataSource({
+    connector: require("../index"),
+    debug: false,
+    baseURL: 'http://localhost:3000',
+    operations: ['add', 'subtract']});
+
+var model = ds.createModel('dummy');
+
+var app = loopback();
+
+app.use(loopback.rest());
+app.use(server.middleware(server));
+
+// Bind a http interface to the server and let it listen to localhost:3000
+var s = app.listen(3000, function () {
+
+    model.add(1, 2, function(err, data) {
+        console.log(err, data);
+        s.close();
+    });
+
+});
