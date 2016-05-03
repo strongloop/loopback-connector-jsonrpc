6fb035a0305a232647dcb8edc210de048be34317
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 16:53:14 -0700

    update/insert copyright notices

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+// Copyright IBM Corp. 2013. All Rights Reserved.
+// Node module: loopback-connector-jsonrpc
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 module.exports = require('./lib/jsonrpc-connector');
--- test/jsonrpc.test.js
+++ test/jsonrpc.test.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013. All Rights Reserved.
+// Node module: loopback-connector-jsonrpc
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var loopback = require("loopback");
 var jayson = require('jayson');
 var assert = require('assert');
